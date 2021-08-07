44fdc3a47ee1d3c526ff682b1becd34821df07d8
swarangi263 <swarangisatpute263@gmail.com>
2021-08-07 15:50:40 +0530

    directory and database

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+// start session
+session_start();
+
+
+// Create constants to store Non Repeating Values
+define('HOME_URL','http://localhost/BE/Website/');
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('LDB_PASSWORD','');
+define('DB_NAME','canteen_automation');
+
+$conn = mysqli_connect("localhost","root","") or die(mysqli_error($conn)); //db connection
+    
+mysqli_select_db($conn,"canteen_automation") or die(mysqli_error($conn)); //selecting db
+
+
+?>
+
