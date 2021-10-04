1636dbe3557ff34664fb3dab8d34334d11f2f2db
swarangisatpute263 <swarangisatpute263@example.com>
2021-10-04 21:13:19 +0530

    committed the config

--- config/constants.php
+++ config/constants.php
@@ -9,6 +9,11 @@ define('LOCALHOST','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
 define('DB_NAME','canteen_automation');
+// define('HOME_URL','http://localhost/Canteen_Automation-main/');
+// define('HOST','sql304.iceiy.com');
+// define('DB_USERNAME','icei_29920603');
+// define('DB_PASSWORD','CAS@1234');
+// define('DB_NAME','icei_29920603_canteen_automation');
 
 $conn = mysqli_connect("localhost","root","") or die(mysqli_error($conn)); //db connection
     
